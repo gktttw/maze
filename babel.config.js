420e149f112117c3a83359c1c1bd991f0921cbba
gktttw <gktttwsam@gmail.com>
2024-07-24 10:58:27 +0800

    try

--- babel.config.js
+++ babel.config.js
@@ -54,12 +54,6 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-transform-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-proposal-private-property-in-object',
         {
           loose: true
